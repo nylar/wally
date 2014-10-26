51f400312b63184fd1165e4267ee762ac5d11cc5
Nylar <me@nylar.io>
2014-10-26 10:24:38 +0000

    go fmt

--- indexer_test.go
+++ indexer_test.go
@@ -3,9 +3,9 @@ package wally
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
-	"os"
 
 	rdb "github.com/dancannon/gorethink"
 	"github.com/stretchr/testify/assert"
@@ -19,13 +19,13 @@ func init() {
 	Database = "testing"
 	DocumentTable = "documents"
 	IndexTable = "indexes"
-	
+
 	var err error
 	session, err = rdb.Connect(rdb.ConnectOpts{
 		Address:  os.Getenv("RETHINKDB_URL"),
 		Database: "test",
 	})
-	
+
 	if err != nil {
 		fmt.Errorf(err.Error())
 	}
@@ -35,11 +35,11 @@ func DbBootstrap() {
 	// Reset database
 	rdb.DbDrop(Database).Exec(session)
 	rdb.DbCreate(Database).Exec(session)
-	
+
 	// Drop tables
 	rdb.Db(Database).TableDrop(DocumentTable).Run(session)
 	rdb.Db(Database).TableDrop(IndexTable).Run(session)
-	
+
 	// Create tables
 	rdb.Db(Database).TableCreate(DocumentTable).Run(session)
 	rdb.Db(Database).TableCreate(IndexTable).Run(session)
@@ -114,7 +114,7 @@ func TestIndexer_SplitTextIntoWords(t *testing.T) {
 
 func TestIndexer_Indexer(t *testing.T) {
 	docId := "12345-67890-ABCDE"
-	
+
 	tests := []struct {
 		Input  interface{}
 		Output []Index
@@ -148,27 +148,27 @@ func TestIndexer_Indexer(t *testing.T) {
 
 func TestIndexer_IndexString(t *testing.T) {
 	indexId := "world"
-	
+
 	index := Index{Id: indexId}
-	
+
 	assert.Equal(t, "Index#world", index.String())
 }
 
 func TestIndexer_IndexPut(t *testing.T) {
 	DbBootstrap()
-	
+
 	index := Index{Id: "hello", Count: 5, DocumentId: "12345-67890-ABCDE"}
-	
+
 	err := index.Put(session)
 	assert.Nil(t, err)
-	
+
 	res, err := rdb.Db(Database).Table(IndexTable).Get("hello").Run(session)
 	assert.Nil(t, err)
-	
+
 	var i Index
 	err = res.One(&i)
 	assert.Nil(t, err)
-	
+
 	assert.Equal(t, i.Id, "hello")
 	assert.Equal(t, i.Count, 5)
 	assert.Equal(t, i.DocumentId, "12345-67890-ABCDE")
@@ -176,30 +176,30 @@ func TestIndexer_IndexPut(t *testing.T) {
 
 func TestIndexer_DocumentString(t *testing.T) {
 	docId := "12345-67890-ABCDE"
-	
+
 	doc := Document{Id: docId}
-	
+
 	assert.Equal(t, "Document#12345-67890-ABCDE", doc.String())
 }
 
 func TestIndexer_DocumentPut(t *testing.T) {
 	DbBootstrap()
-	
+
 	doc := Document{
-		Source: "www.google.com",
+		Source:  "www.google.com",
 		Content: "Lorem ipsum dolor sit amet.",
 	}
-	
+
 	err := doc.Put(session)
 	assert.Nil(t, err)
-	
+
 	res, err := rdb.Db(Database).Table(DocumentTable).Run(session)
 	assert.Nil(t, err)
-	
+
 	var d Document
 	err = res.One(&d)
 	assert.Nil(t, err)
-	
+
 	assert.NotEqual(t, d.Id, "")
 	assert.Equal(t, d.Source, "www.google.com")
 	assert.Equal(t, d.Content, "Lorem ipsum dolor sit amet.")
